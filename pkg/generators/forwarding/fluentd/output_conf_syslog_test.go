7a4b200fe9f545e728a26e0cd03c30701acb17f2
Arik Hadas <ahadas@redhat.com>
2020-01-22 16:39:33 +0200

    support forwarding to remote syslog
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

---
+++ pkg/generators/forwarding/fluentd/output_conf_syslog_test.go
@@ -0,0 +1,53 @@
+package fluentd
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	logging "github.com/openshift/cluster-logging-operator/pkg/apis/logging/v1alpha1"
+	test "github.com/openshift/cluster-logging-operator/test"
+)
+
+var _ = Describe("Generating external syslog server output store config blocks", func() {
+
+	var (
+		err       error
+		outputs   []logging.OutputSpec
+		generator *ConfigGenerator
+	)
+	BeforeEach(func() {
+		generator, err = NewConfigGenerator(false)
+		Expect(err).To(BeNil())
+	})
+
+	Context("for an insecure endpoint", func() {
+		BeforeEach(func() {
+			outputs = []logging.OutputSpec{
+				{
+					Type:     logging.OutputTypeSyslog,
+					Name:     "syslog-receiver",
+					Endpoint: "sl.svc.messaging.cluster.local:9654",
+				},
+			}
+		})
+		It("should produce well formed output label config", func() {
+			results, err := generator.generateOutputLabelBlocks(outputs)
+			Expect(err).To(BeNil())
+			Expect(len(results)).To(Equal(1))
+			test.Expect(results[0]).ToEqual(`<label @SYSLOG_RECEIVER>
+	<match **>
+	   @type copy
+	   <store>
+  	     @type syslog_buffered
+	     @id syslog_receiver
+	     remote_syslog sl.svc.messaging.cluster.local
+	     port 9654
+	     hostname ${hostname}
+	     facility user
+	     severity debug
+	   </store>
+	</match>
+</label>`)
+		})
+	})
+})
